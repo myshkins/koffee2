bd46189a514d40adefbb5f0e61dc4bfed4d5bcb1
myshkins <alex@ak0.io>
2024-02-29 19:33:52 -0500

    fix mobile horizontal

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom"
 
 import Navbar from "../components/Navbar"
+import "./Layout.css"
 
 export default function Layout() {
   return (
-    <>
+    <div className="layout">
       <Navbar />
       <Outlet />
-    </>
+    </div>
   )
 }
